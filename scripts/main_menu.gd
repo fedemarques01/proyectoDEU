22a289e55239e430dcafd620d41bf59091ec5f35
Intimaria <you@example.com>
2024-06-17 21:29:39 -0300

    Options

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,7 +11,7 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/entrada_facu.tscn")
 
 func _on_options_button_pressed():
-	pass
+	get_tree().change_scene_to_file("res://scenes/input_options_menu.tscn")
 
 func _on_tutorial_button_pressed():
 	pass
