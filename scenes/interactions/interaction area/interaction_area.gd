8c05cb8201de3c9cf8573c4e50d460f4dcc0252e
Fabbri, Bautista <qksergbau>
2024-07-17 14:32:33 -0300

    add more interactions falta informacion extra a las interacciones

--- scenes/interactions/interaction area/interaction_area.gd
+++ scenes/interactions/interaction area/interaction_area.gd
@@ -10,12 +10,10 @@ func _ready():
 	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(body):
-	if body.is_in_group("player"):  
-		print("Player entered map area")
+	if body.is_in_group("player"):
 		InteractionManager.register_area(self)
 
 
 func _on_body_exited(body):
-	if body.is_in_group("player"): 
-		print("Player left map area")
+	if body.is_in_group("player"):
 		InteractionManager.unregister_area(self)
