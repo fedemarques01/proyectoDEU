b74ae531aeaeb8c99532afdda6d0de95ec7a69cc
Intimaria <you@example.com>
2024-07-18 00:53:24 -0300

    POC de TTS

--- scripts/global.gd
+++ scripts/global.gd
@@ -8,3 +8,20 @@ var change_camera = false
 #(311, 619) position default for "entrada_facu" start
 var player_transition_posx = 311
 var player_transition_posy = 619
+
+
+# TTS variables
+var tts_enabled = true
+var voices = DisplayServer.tts_get_voices_for_language("es")
+var voice_id = voices[4]
+
+func _ready():
+	print("Global listo: TTS inicializado")
+
+
+func speak(text: String):
+	if tts_enabled:
+		DisplayServer.tts_speak(text, voice_id)
+
+func test_start():
+	speak("Bienvenido a la entrada de la universidad")
