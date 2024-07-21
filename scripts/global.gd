c826611d6960f5dd5f277c82157c0b4f02dc6175
Intimaria <you@example.com>
2024-07-21 13:18:56 -0300

    Fix tamaño, add funcionalidad y titulo opciones

--- scripts/global.gd
+++ scripts/global.gd
@@ -5,6 +5,9 @@ var transition_scene = false
 var transition_to = "entrada_facu"
 var change_camera = false
 
+var font_size: int = 14
+var font_color: Color = Color.WHITE
+
 #(311, 619) position default for "entrada_facu" start
 var player_transition_posx = 311
 var player_transition_posy = 619
