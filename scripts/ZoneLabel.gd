d9722cf34b39f2feaff95d5453ba6b2689b09d4d
Fabbri, Bautista <qksergbau>
2024-07-31 00:20:58 -0300

    fix capitalize first word

--- scripts/ZoneLabel.gd
+++ scripts/ZoneLabel.gd
@@ -7,5 +7,10 @@ func _ready(): #tomo el nombre desde current_scene, lo edito para que sea legibl
 	zoneName =  zoneName.replace("2","inferior")
 	
 	#cargo el label e inicio la animacion
-	$AnimationPlayer/CanvasLayer/Label.text = zoneName
+	$AnimationPlayer/CanvasLayer/Label.text = capitalize_first_word(zoneName)
 	$AnimationPlayer.play("fade")
+
+func capitalize_first_word(text: String) -> String: #mayuscula a la primer palabra
+	if text.length() == 0:
+		return text
+	return text.substr(0, 1).to_upper() + text.substr(1)
