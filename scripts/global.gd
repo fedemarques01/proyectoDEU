76eaffcbbebfd06d7cae0ae26735ee5c89c5ea08
Intimaria <you@example.com>
2024-08-05 15:36:31 -0300

    fix: estado de velocidad

--- scripts/global.gd
+++ scripts/global.gd
@@ -5,13 +5,19 @@ var transition_scene = false
 var transition_to = "entrada_facu"
 var change_camera = false
 
+
+# configs
+const CONFIG_PATH: String = "user://settings.cfg"
 var font_size: int = 14
 var font_color: Color = Color.WHITE
 
 #(311, 619) position default for "entrada_facu" start
 var player_transition_posx = 311
 var player_transition_posy = 619
-var SPEED = 400.0
+
+# Velocidad del jugador
+var initial_speed: float = 400.0
+var SPEED: float = initial_speed
 
 var pause_menu
 # TTS variables
@@ -26,12 +32,27 @@ func _ready():
 	get_tree().root.call_deferred("add_child", pause_menu) 
 	pause_menu.process_mode = ProcessMode.PROCESS_MODE_ALWAYS
 	pause_menu.hide() 
+	load_speed()
 
-func get_speed():
+func get_speed() -> float:
 	return SPEED
-func set_speed(speed):
+func set_speed(speed: float):
 	SPEED = speed
-
+	save_speed()
+func reset_speed():
+	SPEED = self.initial_speed
+func load_speed():
+	var config = ConfigFile.new()
+	var err = config.load(CONFIG_PATH)
+	if err == OK:
+		SPEED = config.get_value("player", "speed", initial_speed)
+	else:
+		SPEED = initial_speed
+func save_speed():
+	var config = ConfigFile.new()
+	config.set_value("player", "speed", SPEED)
+	config.save(CONFIG_PATH)
+	
 func speak(text: String):
 	if tts_enabled:
 		DisplayServer.tts_speak(text, voice_id)
